d39d6ef1528d3818c1f1377ddfec1f724693cd9e
osolaru1 <59890727+osolaru1@users.noreply.github.com>
2020-02-25 11:50:13 -0500

    Add files via upload

---
+++ InClassActivity5.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<style>
+p.a{
+font-style:italic;	
+	
+}
+
+
+
+</style>
+<body>
+
+
+
+<p class = "a">“Good morning, Dave,” said HAL.”</p>
+
+
+<?php
+
+function CircleArea($radius) 
+{
+return M_PI*pow($radius,2);
+ echo "The Circle Area is: "$sol; 
+}
+
+
+echo("The Circle Area is "); 
+echo(CircleArea(5)); 
+return 0;
+?> 
+
+</body>
+</html>
